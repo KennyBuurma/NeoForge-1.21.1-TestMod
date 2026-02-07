a625b0836b4b4b73e47dbfd0b5c8bb6bdae1f5ea
Kenny <kenny.d.buurma@gmail.com>
2026-02-07 14:47:01 +0100

    Commit 0.2 - Setup for worldgen

---
+++ src/main/java/net/Kenny/testmod/worldgen/ModPlacedFeatures.java
@@ -0,0 +1,28 @@
+package net.Kenny.testmod.worldgen;
+
+import net.Kenny.testmod.TestMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+
+public class ModPlacedFeatures {
+
+    public static void bootstrap(BootstrapContext<PlacedFeature> context) {
+        var configuredFeatured = context.lookup(Registries.CONFIGURED_FEATURE);
+
+    }
+
+    public static ResourceKey<PlacedFeature> registerKey(String name) {
+        return ResourceKey.create(Registries.PLACED_FEATURE, ResourceLocation.fromNamespaceAndPath(TestMod.MOD_ID, name));
+    }
+
+    private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstrapContext<ConfiguredFeature<?, ?>> context,
+                                                                                          ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
+        context.register(key, new ConfiguredFeature<>(feature, configuration));
+    }
+}
